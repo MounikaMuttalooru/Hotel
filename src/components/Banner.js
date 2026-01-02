e743f1d21b3f89946cbbcd50024f2839b5673708
MounikaMuttalooru <mmuttalooru@gmail.com>
2026-01-01 22:11:05 -0500

    room type updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import bannerImg from '../bannerimage.jpg';
 
 export default function Banner() {
-  const url = 'https://images.unsplash.com/photo-1501117716987-c8e0b6b4f0b0?auto=format&fit=crop&w=1400&q=60';
   return (
-    <div className="banner" style={{ backgroundImage: `url(${url})` }}>
+    <div className="banner" style={{ backgroundImage: `url(${bannerImg})` }}>
       <h1 style={{ textShadow: '0 2px 8px rgba(0,0,0,0.6)' }}>Find your perfect stay</h1>
     </div>
   );
